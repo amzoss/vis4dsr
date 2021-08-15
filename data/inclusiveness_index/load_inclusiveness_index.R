9a2de09eab02da09f00232275f4b48f550dc601e
amzoss <angela.zoss@gmail.com>
2021-08-15 14:39:49 -0400

    rebuild site

--- data/inclusiveness_index/load_inclusiveness_index.R
+++ data/inclusiveness_index/load_inclusiveness_index.R
@@ -3,4 +3,10 @@ library(readxl)
 
 inclusiveness_index <- read_excel('data/inclusiveness_index/global_data_for_website_2020.xlsx',
                                   na="9999") %>%
-  rename_with(~ str_remove_all(.x, "[(|)]") %>% str_replace_all("[-| ]", "."))
+  rename_with(~ str_remove_all(.x, "[(|)]") %>% str_replace_all("[-| ]", ".")) %>%
+  dplyr::filter(Continent != "Antarctica")
+
+inclusiveness_index_annual <- read_excel('data/inclusiveness_index/inclusiveness_global_data_2016_2020.xlsx',
+                                  na="9999") %>%
+  rename_with(~ str_remove_all(.x, "[(|)]") %>% str_replace_all("[-| ]", ".")) %>%
+  dplyr::filter(Continent != "Antarctica")
